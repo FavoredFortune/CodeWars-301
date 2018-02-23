55a0e60bccb4e1f18f269256efe1a31e59332284
Suzanne Richman <suzanne@favoredfortune.com>
2018-02-23 08:46:05 -0800

    working on pin

--- CW05/Validate-PIN/solutions.js
+++ CW05/Validate-PIN/solutions.js
@@ -5,9 +5,12 @@
 function validatePIN (pin) {
 
   //the \d is meant to match only digits, so if a string has something other than digits it returns false
-  // {4| 6} is meant to match strings that are only 4 or 6 characters in length
-  //each works for its own part, but the two don't work together  WEIRD
+  // {4} and {6} are meant to match strings that are only 4 or 6 characters in length
+  // the | is meant to make it either 4 or 6 digits
+  //each works for its own part, but the two don't work together 
+  //currently passing 51 failing 7
 
-  let res =  /^([\d{4|6}])$/.test(pin);
-  return  res ? true : false; 
+  let res =  /^([\d]{4}|[\d]{6}])/g.test(pin);
+  return res ? true : false;
 }
+
