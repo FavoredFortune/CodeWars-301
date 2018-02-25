86df19750b7368894724f79033b9f731dfe808d0
Suzanne Richman <suzanne@favoredfortune.com>
2018-02-24 21:02:25 -0800

    finally working

--- CW05/alt-cap/solutions.js
+++ CW05/alt-cap/solutions.js
@@ -2,14 +2,28 @@
 
 //This codewar challenge was to create two strings from one with alternate capitialization in each based on even/odd indicies
 
-//not working, failed all tests, but did create an array of two identical strings! 
+//not working, failed all tests, but did create an array of two identical strings!
 
 function capitalize(s){
-  var stringArray = [s,s];
-  var stringA = stringArray[0];
-  stringA.toupperCase();
-  var stringB = stringArray[1];
-  stringB.toUpperCase();
-  stringArray=[stringA,stringB];
-  return stringArray;
+  let stringArray1 = [];
+  let stringArray2 = [];
+  let capitalArray = [];
+
+  for(var i = 0; i < s.length; i++){
+    if (i % 2 === 0) {
+      stringArray1[i] = s[i].toUpperCase();
+    }else {
+      stringArray1[i] = s[i].toLowerCase();
+    }
+
+    if (i % 2 !== 0) {
+      stringArray2[i] = s[i].toUpperCase();
+    }else {
+      stringArray2[i] = s[i].toLowerCase();
+    }
+  }
+  capitalArray.push(stringArray1.join(''));
+  capitalArray.push(stringArray2.join(''));
+
+  return capitalArray;
 }
