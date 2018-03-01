4c43500b168b77e5e09444990953aba0d730daa6
Suzanne Richman <suzanne@favoredfortune.com>
2018-03-01 09:45:27 -0800

    realized I left my solution in progress in the Kata and not in my file - WHOOPS

--- CW13/mutate-strings/solutions.js
+++ CW13/mutate-strings/solutions.js
@@ -1,4 +1,23 @@
 'use strict';
 
-//To mutate one string into another string one letter at a time, starting with the first letter first. 
+//To mutate one string into another string one letter at a time, starting with the first letter first.
+
+function mutateMyStrings(stringOne, stringTwo){
+
+  //for loop with first string replacing one element at a time from string two into string one
+
+  for (let i in stringOne){
+    let trimString = stringOne[i].remove();
+    let returnString = stringTwo[i].replace('z');
+    console.log(returnString);
+
+    let mutatedString = returnString + stringOne + '\n';
+
+    console.log(mutatedString);
+  }
+  //return the string with each loop adding a \n with concatenation to give the returns expected as in the test
+
+
+
+}
 
